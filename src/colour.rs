7ae8d61e77ac0b6d13cfa3e65e1bd698db3d042d
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2022-09-05 21:06:50 +0100

    Added colour parsing

--- src/colour.rs
+++ src/colour.rs
@@ -1,3 +1,5 @@
+use crate::config::ParseError;
+
 #[derive(Debug)]
 pub(crate) struct Colour {
     r: u8,
@@ -16,4 +18,19 @@ pub(crate) fn colour_string(string: String, colour: Colour) -> String {
         "\x1b[38;2;{};{};{}m{}\x1b[0m\n",
         colour.r, colour.g, colour.b, string
     )
+}
+
+impl TryFrom<String> for Colour {
+    type Error = ParseError;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        if s.len() != 6 {
+            return Err(ParseError::InvalidColour(s));
+        }
+        let hex = u32::from_str_radix(&s, 16).or(Err(ParseError::InvalidColour(s)))?;
+        let r = ((hex & 0xFF0000) >> 16) as u8;
+        let g = ((hex & 0x00FF00) >> 8) as u8;
+        let b = (hex & 0x0000FF) as u8;
+        Ok(Colour::new(r, g, b))
+    }
 }
