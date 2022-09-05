e455c2546e6b47ab42c01de6e09d693d2fd0720e
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2022-09-05 21:07:27 +0100

    Added more parse errors

--- src/config.rs
+++ src/config.rs
@@ -7,27 +7,29 @@ use serde::Deserialize;
 pub(crate) type Config = BTreeMap<String, Profile>;
 pub(crate) type Profile = BTreeMap<String, Segment>;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub(crate) struct Segment {
     identifiers: Vec<String>,
     colours: Vec<String>,
 }
 
 #[derive(Debug)]
-pub(crate) enum ConfigError {
+pub(crate) enum ParseError {
     MismatchedIdentColours {
         profile_name: String,
         segment_name: String,
     },
+    InvalidRegex(String),
+    InvalidColour(String),
     CouldNotParse(serde_yaml::Error),
 }
 
-impl std::error::Error for ConfigError {}
+impl std::error::Error for ParseError {}
 
-impl fmt::Display for ConfigError {
+impl fmt::Display for ParseError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            ConfigError::MismatchedIdentColours {
+            ParseError::MismatchedIdentColours {
                 segment_name,
                 profile_name,
             } => write!(
@@ -35,7 +37,9 @@ impl fmt::Display for ConfigError {
                 "Segment '{}' must have the same number of colours and identifiers in profile '{}'",
                 segment_name, profile_name
             ),
-            ConfigError::CouldNotParse(error) => {
+            ParseError::InvalidRegex(r) => write!(f, "Invalid regex: {}", r),
+            ParseError::InvalidColour(c) => write!(f, "Invalid colour: {}", c),
+            ParseError::CouldNotParse(error) => {
                 write!(f, "Couldn't parse config, error: {}", error)
             }
         }
@@ -46,18 +50,26 @@ impl Segment {
     pub fn is_valid(&self) -> bool {
         self.identifiers.len() == self.colours.len()
     }
+
+    pub(crate) fn identifiers(&self) -> &[String] {
+        self.identifiers.as_ref()
+    }
+
+    pub(crate) fn colours(&self) -> &[String] {
+        self.colours.as_ref()
+    }
 }
 
-pub(crate) fn parse_config_from_reader<R>(reader: R) -> Result<Config, ConfigError>
+pub(crate) fn parse_config_from_reader<R>(reader: R) -> Result<Config, ParseError>
 where
     R: io::Read,
 {
-    let cfg: Config = serde_yaml::from_reader(reader).map_err(|e| ConfigError::CouldNotParse(e))?;
+    let cfg: Config = serde_yaml::from_reader(reader).map_err(|e| ParseError::CouldNotParse(e))?;
 
     for (name, profile) in cfg.iter() {
         for (k, v) in profile.iter() {
             if !v.is_valid() {
-                return Err(ConfigError::MismatchedIdentColours {
+                return Err(ParseError::MismatchedIdentColours {
                     profile_name: name.clone(),
                     segment_name: k.clone(),
                 });
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
 use colour::Colour;
 
-mod config;
 mod colour;
-
+mod config;
+mod matcher;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let f = std::fs::File::open("example_conf.yaml")?;
-    let cfg = config::parse_config_from_reader(f);
-    match cfg {
-        Ok(cfg) => println!("{:?}", cfg),
-        Err(e) => eprintln!("{}", e),
+    let profile = "logs";
+
+    let cfg = config::parse_config_from_reader(f)?;
+
+    let profile = cfg.get(profile).unwrap();
+    let mut all_matchers = Vec::new();
+    for segment in profile.values() {
+        let mut matchers = Vec::<matcher::Matcher>::try_from(segment.clone())?;
+        all_matchers.append(&mut matchers);
     }
 
-    println!("{}", colour::colour_string("hello there".to_string(), colour::Colour::new(0, 255, 255)));
+    println!(
+        "{}",
+        colour::colour_string("hello there".to_string(), colour::Colour::new(0, 255, 255))
+    );
 
     Ok(())
 }
